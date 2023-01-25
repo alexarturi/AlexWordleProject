3905c092b7cd4918e48b150cbf373621fa3a9932
alexarturi <alexandrea8@nycstudents.net>
2023-01-25 17:13:49 -0500

    Wordle Woodstock

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,46 @@
+import java.util.Scanner;
+
 public class Main {
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RESET = "\u001B[0m";
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Wordle game = new Wordle();
+        System.out.println("Welcome to Wordle by Alex Arturi!");
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("The word is: " + game.getWord());
+
+        int counter = 1;
+        String current = "";
+        while (!Wordle.win && counter<=6){
+            while (current.length()!=5){
+                System.out.print("Input your guess " + counter + "/6: ");
+                current = scan.nextLine();
+            }
+            game.checkWord(current);
+            char[] guessArr = current.toCharArray();
+            int c = 0;
+            for (String i : Wordle.colors){
+                if (i.equals("green")){
+                    System.out.print(ANSI_GREEN + guessArr[c] + ANSI_RESET+ " ");
+                    System.out.println("gi");
+                }
+                if (i.equals("yellow")){
+                    System.out.print(ANSI_YELLOW + guessArr[c] + ANSI_RESET + " ");
+                    System.out.println("yi");
+                }
+                if (i.equals("gray")) {
+                    System.out.print(ANSI_BLACK + guessArr[c] + ANSI_RESET+" ");
+                    System.out.println("gri");
+                }
+                c++;
+            }
+            Wordle.win = game.checkWin();
+            counter++;
+        }
+
     }
 }
--- src/Wordle.java
+++ src/Wordle.java
@@ -1,2 +1,42 @@
-package PACKAGE_NAME;public class Wordle {
+public class Wordle {
+    private String[] bank = {"HELLO", "IRATE", "ADIEU", "BRAKE", "MOUNT", "HAPPY", "TRACK", "PARTY", "FUDGE", "DROOP"};
+    private String starter;
+    public static String[] colors = {"gray", "gray", "gray", "gray", "gray"};
+    public static boolean win;
+
+    public Wordle(){
+        starter = bank[(int)(Math.random()*10)];
+        win = false;
+    }
+
+    public String getWord(){
+        return starter;
+    }
+
+    public void checkWord(String guess){
+        char[] guessArr = guess.toCharArray();
+        char[] wordArr = starter.toCharArray();
+        for (int i = 0; i<5; i++){
+            if (guessArr[i]==wordArr[i]){
+                colors[i] = "green";
+            } else {
+                if(starter.indexOf(guessArr[i])!=-1) {
+                    colors[i] = "yellow";
+                }
+            }
+        }
+    }
+
+    public boolean checkWin(){
+        int count = 0;
+        for (String i : colors){
+            if (i.equals("green")){
+                count++;
+            }
+        }
+        if (count==5){
+            return true;
+        }
+        return false;
+    }
 }
